1113f57a07055a69c210dee01fa9d7b4b1775acc
VJulie <julie.vong@u-psud.fr>
2015-12-31 01:03:51 +0100

    Update mon_via_navigo_25-12-19h31.py

--- mon_via_navigo_25-12-19h31.py
+++ mon_via_navigo_25-12-19h31.py
@@ -98,14 +98,33 @@ class PublicTransportationNetwork:
         
 
     def load_(self, file_name):
-        file_read = csv.reader(file_name)
-        pass
+        file_read = open(file_name, "rb")
+        reader = csv.reader(file_name)
+        #for row in reader:
+        #Stocker les lignes dans qch mais je ne sais pas quoi
+        file.close()
 
 
 
     def __str__(self):
         txt = ""
         return txt
+        
+    class PasswdDialect(csv.Dialect):
+    # Séparateur de champ
+    delimiter = ":"
+    # Séparateur de ''chaîne''
+    quotechar = None
+    # Gestion du séparateur dans les ''chaînes''
+    escapechar = None
+    doublequote = None
+    # Fin de ligne
+    lineterminator = "\r\n"
+    # Ajout automatique du séparateur de chaîne (pour ''writer'')
+    quoting = csv.QUOTE_NONE
+    # Ne pas ignorer les espaces entre le délimiteur de chaîne
+    # et le texte
+    skipinitialspace = False
 
 
 
