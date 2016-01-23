89b3f5358678fd480b4449a07d7fa68e98326a69
kesavanvillebon36 <k.thirusittampalam@gmail.com>
2016-01-23 21:25:21 +0100

    Update mon_via_navigo_25-12-19h31.py

--- mon_via_navigo_25-12-19h31.py
+++ mon_via_navigo_25-12-19h31.py
@@ -1,13 +1,13 @@
 #################################################
 #Institut Villebon UE 5.I4
 #Projet Via Navigo
-#Auteurs: Mansour Abdoulakime, Kesavan Thirusittampalam et Julie Vong 
+#Auteurs: Mansour Abdoulakime, Kesavan Thirusittampalam et Julie Vong
 #Date de creation: 25/12/15
 #Date de derniere modification: 31/12/15 a 1h36
 #################################################
 import csv
 
-#EN COURS DE MODIFICATION PAR KESAVAN#
+#Station fonctionne c'est bon, testé, mais line je sais pas exactement ce qui est demandé
 
 class Station:
     """
@@ -34,7 +34,7 @@ class Station:
 
 
     def __lt__(self, station):
-        """Decide which station is the smallest in terms of alphabetic 
+        """Decide which station is the smallest in terms of alphabetic
         order of the name, then of the line if it is the same station"""
         if self.name < station.name:
             return self.name #? .name? Bool
@@ -56,8 +56,10 @@ class Station:
 
     def __str__(self):
         """Represent the station"""
-        txt = __hash__(self)
-        txt+= 'Coordinates:'.join(self.position)
+        txt = self.name
+        txt+= ' Coordinates: '+str(self.position)
+        txt+= '\t'
+        txt+= self.in_line
         return txt
 
 
@@ -69,7 +71,7 @@ class Line:
         mode: string (RER, metro, bus or velib)
         name: string"""
     def __init__(self, stations, mode, name):
-        self.stations = self.stations.append(stations) # a été modifie: avant self.stations = stations)
+        self.stations = stations #self.stations.append(stations) # a été modifie: avant self.stations = stations)
         self.mode = mode
         self.name = name
         """Change the attribute in_line of each station of the line"""
@@ -78,7 +80,7 @@ class Line:
 
     def __repr__(self): #suffisant?
         txt = "Line",self,":"
-        txt+= ','.join(station for station in self.stations) 
+        txt+= ','.join(station for station in self.stations)
         return txt
 
 
@@ -88,7 +90,7 @@ class PublicTransportationNetwork:
         stations: list of stations of a line
         node: type of transport : bus, umetro, RER, velib
         name : string, name of the line"""
-        
+
     def __init__(self):
         self.name = ""
         self.scale = float
@@ -97,16 +99,16 @@ class PublicTransportationNetwork:
     def add_a_line(self, line):
         """Add a line the the attributes lines"""
         self.lines.append(line)
-        
+
     def save(self, file_name):
         network = open("network.csv", "w")
-        for 
+
         network.write("toto:10:10") #type/ligne/station/position
         #ligne: line in self.lines
         #station
 
         file_write.close()
-        
+
 
     def load_(self, file_name):
         network = open(file_name, "r")
@@ -118,7 +120,7 @@ class PublicTransportationNetwork:
     def __str__(self):
         txt = ""
         return txt
-        
+
     class MyDialect(csv.Dialect):
         """Define a dialect with ":" as separator instead of default "," """
         # Field separator
